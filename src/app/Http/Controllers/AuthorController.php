3d4eb3b1dc9aba68e738d8a0b9e590bc86c05e12
tangrowth <to.chinatsu@icloud.com>
2023-04-04 12:51:48 +0900

    0-14 データの取得完了

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// Eloquentを使用できるようにAuthorモデルを読み込む
 use App\Models\Author;
+use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
 }
